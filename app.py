62f33309488f31ee89b237bae328e9271df305f5
TalonChen <talonc@nycstudents.net>
2024-09-18 10:03:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-
-
-values = [1,2.23,5,7,2,30,15]
-print (values[1])
+'''values = [1,2.23,5,7,2,30,15]
+print (values)
 for i in values:
-    print(i)
+    print(values [6])'''
+
+x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) 
