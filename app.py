af181f5c9503d932ba98908a9f7cc450e6dc2b10
TalonChen <talonc@nycstudents.net>
2024-09-19 10:11:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-values = [1,2.23,5,7,2,30,15]
-print (values[2])
-for i in values:
-    print(values)
-x = "this is a thing"
-y= x.split( )
-z = y[0]
-print(y)
-print(z) 
+#Typing something into input = String, Anything with ""
+#Search somethings up if you forgot the code or don't know
+#5%2 = 1 If x%2 == 0
+
+x = 100123512412411461126414632423
+
+if x%2 ==0:
+    print("even")
+else:
+    print("odd")
