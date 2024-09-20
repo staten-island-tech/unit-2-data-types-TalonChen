ad3b72c50fa4769782d86b48a41812e61bbaa968
TalonChen <talonc@nycstudents.net>
2024-09-20 10:05:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,30 @@
 #Search somethings up if you forgot the code or don't know
 #5%2 = 1 If x%2 == 0
 
-x = 100123512412411461126414632423
+###Determines if the number is even or odd
+'''
+x = 12412412
 
 if x%2 ==0:
     print("even")
 else:
     print("odd")
+'''
+
+x = 100
+bill = x
+y = ["bad","okay","good","great"]
+service = y
+service = "bad"
+
+
+
+if y(0):
+    print ("0% tip")
+elif y(1):
+    print ("15% tip")
+elif y(2):
+    print ("20% tip")
+else:
+    print ("25% tip")
+
